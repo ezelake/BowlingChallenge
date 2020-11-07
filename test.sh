298d32e21d5c7c4ef57c5b03008b17dff642328c
Ezequiel Lagomarsino <ealagomarsino@teco.com.ar>
2020-11-07 12:39:35 -0300

    Added SH to test with multiple inputs

---
+++ test.sh
@@ -0,0 +1,9 @@
+mvn clean package
+cd target/classes
+for FILE in `ls ../../src/test/input/*.txt`; do
+  echo Test file: `basename $FILE`
+  java ceq/bowling/App $FILE
+  echo Exit code: $?
+  echo "---------------------------------------------------------------------------"
+done
+
